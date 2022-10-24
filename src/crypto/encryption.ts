e2aff9a0c63b79f1d6fd77c5356a70d75f395a4b
achilles schmelzer <achilles@daopanel.com>
2022-10-24 14:25:43 -0700

    Support web worker runtime.
    * In web workers, the web crypto API is available at self.crypto. The
      change here makes it so the library is broken in other environments,
      but for now that's fine because we're going to run only in web workers.
    * Add Buffer and update webpack config to polyfill the Node Buffer API.

--- src/crypto/encryption.ts
+++ src/crypto/encryption.ts
@@ -3,11 +3,7 @@ import Ciphertext, { AESGCMNonceSize, KDFSaltSize } from './Ciphertext'
 
 // crypto should provide access to standard Web Crypto API
 // in both the browser environment and node.
-export const crypto: Crypto =
-  typeof window !== 'undefined'
-    ? window.crypto
-    : // eslint-disable-next-line @typescript-eslint/no-var-requires
-      (require('crypto').webcrypto as unknown as Crypto)
+export const crypto: Crypto = self.crypto
 
 const hkdfNoInfo = new ArrayBuffer(0)
 
